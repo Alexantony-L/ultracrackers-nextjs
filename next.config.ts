9f7ac652971bc5fa8ce89f991efb250f9271cb85
Alex <Alex@gmail.com>
2026-07-06 09:08:15 +0530

    added fix in config ts for pdf generation

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
+   outputFileTracingIncludes: {
+
+    "/api/invoice/route": ["node_modules/@sparticuz/chromium/bin/**"],
+
+  },
   images: {
     remotePatterns: [
       {
